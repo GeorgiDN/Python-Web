da04ab6bfe92c2a6c5759927f6017f786e75e30b
GeorgiDN <go6odn28@gmail.com>
2024-11-19 10:53:43 +0200

    formsAdvanced:Add Comments to post

--- Django-Basics/templatesBasics/forumApp/forumApp/posts/forms.py
+++ Django-Basics/templatesBasics/forumApp/forumApp/posts/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.forms import formset_factory
 
 from forumApp.posts.choises import LanguageChoice
 from forumApp.posts.mixins import DisabledFieldsMixin
-from forumApp.posts.models import Post
+from forumApp.posts.models import Post, Comment
 
 
 class PostBaseForm(forms.ModelForm):
@@ -95,7 +96,48 @@ class SearchForm(forms.Form):
     )
 
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ("author", "content")
 
+        labels = {
+            "author": "",
+            "label": "",
+        }
+
+        error_messages = {
+            "author": {
+                "required": "Author name is required. Write it!",
+            },
+            "content": {
+                "required": "Content is required. Write it!",
+            }
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["author"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Your name",
+        })
+
+        self.fields["content"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Add message...",
+            "rows": 1,
+        })
+
+
+CommentFormSet = formset_factory(CommentForm, extra=1)
+
+
+
+
+
+
+# #######################################################################
 # class PostForm(forms.ModelForm):
 #     title = forms.CharField(
 #         max_length=100,
--- Django-Basics/templatesBasics/forumApp/forumApp/posts/views.py
+++ Django-Basics/templatesBasics/forumApp/forumApp/posts/views.py
@@ -4,7 +4,7 @@ from django.forms import modelform_factory
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from forumApp.posts.forms import PostBaseForm, PostCreateForm, PostDeleteForm, SearchForm
+from forumApp.posts.forms import PostBaseForm, PostCreateForm, PostDeleteForm, SearchForm, CommentFormSet
 from forumApp.posts.models import Post
 
 
@@ -76,9 +76,21 @@ def edit_post(request, pk: int):
 
 def details_page(request, pk: int):
     post = Post.objects.get(pk=pk)
+    formset = CommentFormSet(request.POST or None)
+
+    if request.method == "POST":
+        if formset.is_valid():
+            for form in formset:
+                if form.cleaned_data:
+                    comment = form.save(commit=False)
+                    comment.post = post
+                    comment.save()
+
+            return redirect("details-post", pk=post.id)
 
     context = {
-        "post": post
+        "post": post,
+        "formset": formset,
     }
 
     return render(request, "posts/details-post.html", context)
