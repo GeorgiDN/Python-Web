982fac8a730f4659ca354ed7f17d99cd01b0b6c4
GeorgiDN <go6odn28@gmail.com>
2024-12-01 20:41:32 +0200

    add funtionality: Edit Album

--- Django-Basics/examPrep/albums/views.py
+++ Django-Basics/examPrep/albums/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from albums.forms import AlbumCreateForm
 from albums.models import Album
@@ -15,3 +15,11 @@ class AlbumCreateView(CreateView):
     def form_valid(self, form):
         form.instance.owner = get_user_obj()
         return super().form_valid(form)
+
+
+class AlbumEditView(UpdateView):
+    model = Album
+    form_class = AlbumCreateForm
+    pk_url_kwarg = "id"
+    template_name = "albums/album-edit.html"
+    success_url = reverse_lazy("home")
