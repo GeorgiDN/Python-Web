0497c34039a4321dc73be74333f40784af5dac85
GeorgiDN <go6odn28@gmail.com>
2024-12-12 21:02:53 +0200

    feat: change FBV to CBV

--- Django-Basics/fruitipediaApp/fruitipediaApp/fruits/views.py
+++ Django-Basics/fruitipediaApp/fruitipediaApp/fruits/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, ListView, DeleteView
 
 from fruitipediaApp.fruits.forms import CategoryCreateForm, FruitCreateForm, FruitEditForm, FruitDeleteForm
-from fruitipediaApp.fruits.models import Fruit
+from fruitipediaApp.fruits.models import Fruit, Category
 
 
 def index(request):
     return render(request, 'common/index.html')
 
 
-def dashboard(request):
-    fruits = Fruit.objects.all()
-
-    context = {
-        'fruits': fruits
-    }
-
-    return render(request, 'common/dashboard.html', context)
+class DashboardView(ListView):
+    model = Fruit
+    template_name = 'common/dashboard.html'
+    context_object_name = 'fruits'
 
 
 class CreateFruit(CreateView):
@@ -25,6 +21,72 @@ class CreateFruit(CreateView):
     form_class = FruitCreateForm
     template_name = 'fruits/create-fruit.html'
     success_url = reverse_lazy('dashboard')
+
+
+def details_fruit(request, fruit_id):
+    fruit = Fruit.objects.get(pk=fruit_id)
+
+    context = {
+        'fruit': fruit
+    }
+
+    return render(request, 'fruits/details-fruit.html', context)
+
+
+class FruitEditView(UpdateView):
+    model = Fruit
+    form_class = FruitEditForm
+    template_name = 'fruits/edit-fruit.html'
+    success_url = reverse_lazy('dashboard')
+
+    def get_object(self, queryset=None):
+        fruit_id = self.kwargs.get('fruit_id')
+        return Fruit.objects.get(pk=fruit_id)
+
+
+class FruitDeleteView(DeleteView):
+    model = Fruit
+    template_name = 'fruits/delete-fruit.html'
+    context_object_name = 'fruit'
+    success_url = reverse_lazy('dashboard')
+    form_class = FruitDeleteForm
+
+    def get_initial(self):
+        return self.get_object().__dict__
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({
+            "data": self.get_initial()
+        })
+
+        return kwargs
+
+    def get_object(self, queryset=None):
+        # Use the fruit_id from the URL and get the object
+        return Fruit.objects.get(pk=self.kwargs['fruit_id'])
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryCreateForm
+    template_name = 'categories/create-category.html'
+    success_url = reverse_lazy('create category')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        self.object = None
+        return response
+
+
+# def dashboard(request):
+#     fruits = Fruit.objects.all()
+#
+#     context = {
+#         'fruits': fruits
+#     }
+#
+#     return render(request, 'common/dashboard.html', context)
 
 
 # def create_fruit(request):
@@ -44,27 +106,6 @@ class CreateFruit(CreateView):
 #
 #     return render(request, 'fruits/create-fruit.html', context)
 
-
-def details_fruit(request, fruit_id):
-    fruit = Fruit.objects.get(pk=fruit_id)
-
-    context = {
-        'fruit': fruit
-    }
-
-    return render(request, 'fruits/details-fruit.html', context)
-
-
-class FruitEditView(UpdateView):
-    model = Fruit
-    form_class = FruitEditForm
-    template_name = 'fruits/edit-fruit.html'
-    context_object_name = 'fruit'
-    success_url = reverse_lazy('dashboard')
-
-    def get_object(self, queryset=None):
-        fruit_id = self.kwargs.get('fruit_id')
-        return Fruit.objects.get(pk=fruit_id)
 
 # def edit_fruit(request, fruit_id):
 #     fruit = Fruit.objects.get(pk=fruit_id)
@@ -87,39 +128,39 @@ class FruitEditView(UpdateView):
 #     return render(request, 'fruits/edit-fruit.html', context)
 
 
-def delete_fruit(request, fruit_id):
-    fruit = Fruit.objects.get(pk=fruit_id)
+# def delete_fruit(request, fruit_id):
+#     fruit = Fruit.objects.get(pk=fruit_id)
+#
+#     if request.method == 'GET':
+#         form = FruitDeleteForm(instance=fruit)
+#     else:
+#         form = FruitDeleteForm(request.POST, instance=fruit)
+#
+#         if form.is_valid():
+#             fruit.delete()
+#
+#         return redirect('dashboard')
+#
+#     context = {
+#         'form': form,
+#         'fruit': fruit,
+#     }
+#
+#     return render(request, 'fruits/delete-fruit.html', context)
 
-    if request.method == 'GET':
-        form = FruitDeleteForm(instance=fruit)
-    else:
-        form = FruitDeleteForm(request.POST, instance=fruit)
 
-        if form.is_valid():
-            fruit.delete()
-
-        return redirect('dashboard')
-
-    context = {
-        'form': form,
-        'fruit': fruit,
-    }
-
-    return render(request, 'fruits/delete-fruit.html', context)
-
-
-def create_category(request):
-    if request.method == 'GET':
-        form = CategoryCreateForm()
-    else:
-        form = CategoryCreateForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-        form = CategoryCreateForm()
-
-    context = {
-        'form': form
-    }
-
-    return render(request, 'categories/create-category.html', context)
+# def create_category(request):
+#     if request.method == 'GET':
+#         form = CategoryCreateForm()
+#     else:
+#         form = CategoryCreateForm(request.POST)
+#
+#         if form.is_valid():
+#             form.save()
+#         form = CategoryCreateForm()
+#
+#     context = {
+#         'form': form
+#     }
+#
+#     return render(request, 'categories/create-category.html', context)
