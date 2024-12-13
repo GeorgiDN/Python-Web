78eb1c450d6305a76c93e17c3b360a9e8279cb8d
GeorgiDN <go6odn28@gmail.com>
2024-12-13 20:22:39 +0200

    feat: add logout function

--- Django-Basics/templatesBasics/forumApp/forumApp/accounts/urls.py
+++ Django-Basics/templatesBasics/forumApp/forumApp/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from forumApp.accounts import views
@@ -7,4 +7,5 @@ from forumApp.accounts.views import UserRegisterView
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
