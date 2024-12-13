c77ee06938a84e38cc9fcb1949c2093aa9a37d75
GeorgiDN <go6odn28@gmail.com>
2024-12-13 19:50:21 +0200

    feat: add login function

--- Django-Basics/templatesBasics/forumApp/forumApp/accounts/urls.py
+++ Django-Basics/templatesBasics/forumApp/forumApp/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from forumApp.accounts import views
@@ -5,4 +6,5 @@ from forumApp.accounts.views import UserRegisterView
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
--- Django-Basics/templatesBasics/forumApp/forumApp/settings.py
+++ Django-Basics/templatesBasics/forumApp/forumApp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+
+from django.urls import reverse_lazy
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -156,3 +158,5 @@ MEDIA_ROOT = BASE_DIR/'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+LOGIN_REDIRECT_URL = reverse_lazy('index')
