3cf60de467df394ccc0ae7242c343673dc8c2010
GeorgiDN <go6odn28@gmail.com>
2024-12-17 08:54:59 +0200

    feat: CBV example

--- Django-Advanced/libraryApi/libraryApi/books/urls.py
+++ Django-Advanced/libraryApi/libraryApi/books/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from libraryApi.books import views
 
 urlpatterns = [
-    path('books/', views.list_books_view, name='books_list'),
+    path('books/', views.ListBooksView.as_view(), name='books_list'),
+    path('book/<int:pk>/', views.BookViewSet.as_view(), name='books_viewset'),
 ]
--- Django-Advanced/libraryApi/libraryApi/books/views.py
+++ Django-Advanced/libraryApi/libraryApi/books/views.py
@@ -1,8 +1,9 @@
 from django.http import JsonResponse
-from django.shortcuts import render
-from rest_framework import status
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from libraryApi.books.models import Book
 from libraryApi.books.serializers import BookSerializer
@@ -25,3 +26,55 @@ def list_books_view(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ListBooksView(APIView):
+
+    def get(self, request):
+        books = Book.objects.all()
+
+        serializer = BookSerializer(books, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BookViewSet(APIView):
+    @staticmethod
+    def get_object(pk):
+        return get_object_or_404(Book, pk=pk)
+
+    @staticmethod
+    def serializer_valid(serializer):
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, pk: int):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk: int):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book, data=request.data)
+        return self.serializer_valid(serializer)
+
+    def patch(self, request, pk: int):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book, data=request.data, partial=True)
+        return self.serializer_valid(serializer)
+
+    def delete(self, request, pk: int):
+        book = self.get_object(pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
