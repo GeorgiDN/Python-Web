ae8f014710b14ed0545e8edc5c8d049968670618
GeorgiDN <go6odn28@gmail.com>
2025-04-01 07:28:49 +0300

    feat(comments and likes) add comment and like model

--- Django-Basics/Django-basics-projects/petstagram/petstagram/common/models.py
+++ Django-Basics/Django-basics-projects/petstagram/petstagram/common/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from petstagram.photos.models import Photo
 
-# Create your models here.
+
+class Comment(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=['date_time_of_publication']),
+        ]
+        ordering = ['-date_time_of_publication']
+
+    text = models.TextField(
+        max_length=300,
+    )
+    date_time_of_publication = models.DateTimeField(
+        auto_now_add=True,
+    )
+    to_photo = models.ForeignKey(
+        to=Photo,
+        on_delete=models.CASCADE,
+        related_name='photo_comments',
+    )
+
+
+class Like(models.Model):
+    to_photo = models.ForeignKey(
+        to=Photo,
+        on_delete=models.CASCADE,
+        related_name="photo_likes",
+    )
