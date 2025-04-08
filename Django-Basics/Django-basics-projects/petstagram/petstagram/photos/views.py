a7444faac3ab6d9e422ca2ebea1cc63ef586d8ad
GeorgiDN <go6odn28@gmail.com>
2025-04-08 19:25:49 +0300

    refactor(photos): make view with CBV

--- Django-Basics/Django-basics-projects/petstagram/petstagram/photos/views.py
+++ Django-Basics/Django-basics-projects/petstagram/petstagram/photos/views.py
@@ -1,55 +1,38 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DetailView
 
 from petstagram.common.forms import CommentForm
 from petstagram.photos.forms import PhotoAddForm, PhotoEditForm
 from petstagram.photos.models import Photo
 
 
-def photo_add_page(request):
-    form = PhotoAddForm(request.POST or None, request.FILES or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'photos/photo-add-page.html', context)
+class PhotoAddPage(CreateView):
+    model = Photo
+    template_name = 'photos/photo-add-page.html'
+    form_class = PhotoAddForm
+    success_url = reverse_lazy('home')
 
 
-def photo_edit_page(request, pk: int):
-    photo = Photo.objects.get(pk=pk)
-    form = PhotoEditForm(request.POST or None, instance=photo)
+class PhotoEditPage(UpdateView):
+    model = Photo
+    form_class = PhotoEditForm
+    template_name = 'photos/photo-edit-page.html'
 
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('photo-details', pk=pk)
-
-    context = {
-        'form': form,
-        'photo': photo,
-    }
-
-    return render(request, 'photos/photo-edit-page.html', context)
+    def get_success_url(self):
+        return reverse_lazy('photo-details', kwargs={'pk': self.object.pk})
 
 
-def photo_details_page(request, pk: int):
-    photo = Photo.objects.get(pk=pk)
-    likes = photo.photo_likes.all()
-    comments = photo.photo_comments.all()
-    comment_form = CommentForm()
+class PhotoDetailsView(DetailView):
+    model = Photo
+    template_name = 'photos/photo-details-page.html'
 
-    context = {
-        'photo': photo,
-        'likes': likes,
-        'comments': comments,
-        'comment_form': comment_form,
-    }
-
-    return render(request, 'photos/photo-details-page.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['likes'] = self.object.photo_likes.all()
+        context['comments'] = self.object.photo_comments.all()
+        context['comment_form'] = CommentForm()
+        return context
 
 
 def photo_delete(request, pk):
@@ -57,3 +40,51 @@ def photo_delete(request, pk):
     photo.delete()
 
     return redirect("home")
+
+
+###################################### FBV #########################################
+# def photo_add_page(request):
+#     form = PhotoAddForm(request.POST or None, request.FILES or None)
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#
+#     context = {
+#         'form': form,
+#     }
+#
+#     return render(request, 'photos/photo-add-page.html', context)
+
+
+# def photo_edit_page(request, pk: int):
+#     photo = Photo.objects.get(pk=pk)
+#     form = PhotoEditForm(request.POST or None, instance=photo)
+#
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             form.save()
+#             return redirect('photo-details', pk=pk)
+#
+#     context = {
+#         'form': form,
+#         'photo': photo,
+#     }
+#
+#     return render(request, 'photos/photo-edit-page.html', context)
+
+
+# def photo_details_page(request, pk: int):
+#     photo = Photo.objects.get(pk=pk)
+#     likes = photo.photo_likes.all()
+#     comments = photo.photo_comments.all()
+#     comment_form = CommentForm()
+#
+#     context = {
+#         'photo': photo,
+#         'likes': likes,
+#         'comments': comments,
+#         'comment_form': comment_form,
+#     }
+#
+#     return render(request, 'photos/photo-details-page.html', context)
