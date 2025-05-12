0b8ab85965b8944f2a763f1d647fc2807dc08df2
GeorgiDN <go6odn28@gmail.com>
2025-05-12 09:06:50 +0300

    feat: add frond end for CRUD operations with books

--- Django-Advanced/libraryApi2/libraryApi2/settings.py
+++ Django-Advanced/libraryApi2/libraryApi2/settings.py
@@ -28,8 +28,16 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500',
+    'http://localhost:5500',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -44,6 +52,7 @@ INSTALLED_APPS = [
 
     'rest_framework',  # pip install djangorestframework
     'drf_spectacular', # for swagger
+    'corsheaders',  # pip install django-cors-headers
 ]
 
 REST_FRAMEWORK = {
@@ -61,6 +70,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
