78963b3e61a283935a0ea0e024e93370a3bd63be
GeorgiDN <go6odn28@gmail.com>
2025-06-01 14:08:29 +0300

    feat: profile details

--- Django-Basics/Django-exams/tastyApp/tastyApp/accounts/views.py
+++ Django-Basics/Django-exams/tastyApp/tastyApp/accounts/views.py
@@ -1,8 +1,13 @@
+from django.db.models import Sum, Count
+from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import CreateView
 
 from tastyApp.accounts.forms import ProfileCreateForm
 from tastyApp.accounts.models import Profile
+from tastyApp.core.utils import get_recipies, get_profile
+from tastyApp.recipie.models import Recipie
 
 
 class ProfileCreateView(CreateView):
@@ -10,3 +15,22 @@ class ProfileCreateView(CreateView):
     form_class = ProfileCreateForm
     template_name = 'profile/create-profile.html'
     success_url = reverse_lazy('catalogue')
+
+
+class ProfileDetailView(View):
+    template_name = 'profile/details-profile.html'
+
+    def get(self, request, *args, **kwargs):
+        profile = get_profile()
+        recipes = get_recipies()
+        total_recipes = Recipie.objects.filter(author=profile).aggregate(
+            total=Count('id')
+        )['total']
+
+        context = {
+            'profile': profile,
+            'recipes': recipes,
+            'total_recipes': total_recipes,
+        }
+
+        return render(request, self.template_name, context)
