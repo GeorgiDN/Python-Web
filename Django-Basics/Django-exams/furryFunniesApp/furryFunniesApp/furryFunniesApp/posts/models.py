b71451683313439d68fb8b5ae0df5f7f7a217cf9
GeorgiDN <go6odn28@gmail.com>
2025-06-16 20:10:40 +0300

    feat: add models for posts

--- Django-Basics/Django-exams/furryFunniesApp/furryFunniesApp/furryFunniesApp/posts/models.py
+++ Django-Basics/Django-exams/furryFunniesApp/furryFunniesApp/furryFunniesApp/posts/models.py
@@ -1,3 +1,27 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from furryFunniesApp.accounts.models import Author
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=50,
+        validators=[MinLengthValidator(5)],
+        error_messages={
+            'unique': "Oops! That title is already taken. How about something fresh and fun?",
+        }
+    )
+    image = models.URLField(
+        help_text="Share your funniest furry photo URL!"
+    )
+    content = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        to=Author,
+        on_delete=models.CASCADE,
+        related_name='profile_posts',
+    )
+
+    def __str__(self):
+        return self.title
