df4d30ba8e3dc11fae697b039a8595b6dfbdec9e
GeorgiDN <go6odn28@gmail.com>
2025-06-17 07:49:18 +0300

    feat: author details

--- Django-Basics/Django-exams/furryFunniesApp/furryFunniesApp/furryFunniesApp/accounts/urls.py
+++ Django-Basics/Django-exams/furryFunniesApp/furryFunniesApp/furryFunniesApp/accounts/urls.py
@@ -4,7 +4,7 @@ from furryFunniesApp.accounts import views
 
 urlpatterns = [
     path('create/', views.ProfileCreateView.as_view(), name='profile-create'),
-    # path('details/', views.ProfileDetailView.as_view(), name='profile-details'),
+    path('details/', views.ProfileDetailView.as_view(), name='profile-details'),
     # path('edit/', views.ProfileEditView.as_view(), name='profile-edit'),
     # path('delete/', views.ProfileDeleteView.as_view(), name='profile-delete'),
 ]
--- Django-Basics/Django-exams/furryFunniesApp/furryFunniesApp/furryFunniesApp/accounts/views.py
+++ Django-Basics/Django-exams/furryFunniesApp/furryFunniesApp/furryFunniesApp/accounts/views.py
@@ -1,7 +1,12 @@
+from django.db.models import Count
+from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import CreateView
 from furryFunniesApp.accounts.models import Author
 from furryFunniesApp.accounts.forms import ProfileCreateForm
+from furryFunniesApp.core.utils import get_profile, get_posts
+from furryFunniesApp.posts.models import Post
 
 
 class ProfileCreateView(CreateView):
@@ -9,3 +14,22 @@ class ProfileCreateView(CreateView):
     form_class = ProfileCreateForm
     template_name = 'accounts/create-author.html'
     success_url = reverse_lazy('dashboard')
+
+
+class ProfileDetailView(View):
+    template_name = 'accounts/details-author.html'
+
+    def get(self, request, *args, **kwargs):
+        profile = get_profile()
+        posts = get_posts()
+        total_posts = Post.objects.filter(author=profile).aggregate(total=Count('id'))['total']
+        last_updated_post = Post.objects.filter(author=profile).order_by('-updated_at').first()
+
+        context = {
+            'profile': profile,
+            'posts': posts,
+            'total_posts': total_posts,
+            'last_updated_post': last_updated_post
+        }
+
+        return render(request, self.template_name, context)
